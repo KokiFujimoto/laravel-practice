e6349188227464a31367914e4e886508f1430bf2
Fujimoto <gegvp0330@gmail.com>
2021-11-10 17:57:40 +0900

    change https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,6 +29,7 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
         if (\App::environment(['production'])) {
             \URL::forceSchema('https');
+            
         }
     }
 }
