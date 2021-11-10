2f001168de055973153b60ed24c80b75a8dc8e8e
Fujimoto <gegvp0330@gmail.com>
2021-11-10 16:35:32 +0900

    change app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,8 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}">
-
 <title>@yield('title')</title>
-
 <script src="{{ asset('js/app.js') }}" defer></script>
-
 <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 
@@ -18,7 +15,7 @@
             <div class="container">
             <a href="{{ url('/') }}" class="navbar-brand">
                     {{ config('app.name', 'Laravel') }}
-                </a>
+            </a>
             </div>
             <main class="py-4 ">
                 @yield('content')
