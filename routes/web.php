f67cbda133898c2cc45d4802b232c1da2f6fb02f
Fujimoto <gegvp0330@gmail.com>
2021-11-11 12:25:57 +0900

    change route home

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function(){
-    return view('welcome');
+    return view('index');
 });
 Route::get('/users', [App\Http\Controllers\UserController::class, 'index'])->name('users');
 Route::get('/register', [App\Http\Controllers\UserController::class, 'register'])->name('register');
